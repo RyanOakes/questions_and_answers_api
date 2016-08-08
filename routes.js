7854f5c2348a6f7cc53169df9c6330c52602a28d
oakes <oakesonline@gmail.com>
2016-08-08 15:47:46 -0400

    Set up router for questions and answers

---
+++ routes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require("express");
+var router = express.Router();
+
+// GET /questions
+// Route for questions collection
+router.get("/", function(req, res) {
+    res.json({response: "You sent a GET request"});
+});
+
+// POST /questions
+// Route for creating questions
+router.post("/", function(req, res) {
+    res.json({
+      response: "You sent a POST request",
+      body: req.body
+    });
+});
+
+// GET /questions/:id
+// Route for specific question
+router.get("/:id", function(req, res) {
+    res.json({
+      response: "You sent a GET request for ID " + req.params.id
+    });
+});
+
+module.exports = router;
