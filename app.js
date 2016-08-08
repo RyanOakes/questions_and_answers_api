96fbeec08494932b66901f5f00d70d56a8108cf8
oakes <oakesonline@gmail.com>
2016-08-08 14:41:33 -0400

    Exploring Express' middleware and query features

--- app.js
+++ app.js
@@ -3,6 +3,17 @@
 var express = require("express");
 var app = express();
 
+//linking up middleware
+app.use(function(req, res, next) {
+  console.log("The leaves on the trees are", req.query.color);
+  next();
+});
+
+// app.use(function(req, res, next) {
+//   console.log(req.myMessage);
+//   next();
+// });
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
