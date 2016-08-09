0294753181e0d58120dcf0b0cf577add856b2ed1
oakes <oakesonline@gmail.com>
2016-08-09 13:05:03 -0400

    Added Animal array in sandbox

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -17,7 +17,7 @@ db.once("open", function(){
     var Schema = mongoose.Schema;
     var AnimalSchema = new Schema({
         type: {type: String, default: "goldfish"},
-        size: {type: String, default: "small"},
+        size: String,
         color: {type: String, default: "golden"},
         mass: {type: Number, default: 0.007},
         name: {type: String, default: "Angela"}
@@ -27,7 +27,6 @@ db.once("open", function(){
 
     var elephant = new Animal({
         type: "elepant",
-        size: "big",
         color: "gray",
         mass: 6000,
         name: "Bob"
@@ -37,29 +36,45 @@ db.once("open", function(){
 
     var whale = new Animal({
         type: "whale",
-        size: "huge",
         mass: 190500,
         name: "Tom"
     });
 
+    var animalData = a[
+      {
+          type: "mouse";
+          color: "gray";
+          mass: 0.035,
+          name: "Marvin"
+      },
+      {
+        type: "nutria",
+        color: "brown",
+        mass: 6.35,
+        name: "Gretchen"
+      },
+      {
+        type: "wolf",
+        color: "gray",
+        mass: 45,
+        name: "Iris"
+      },
+      elephant,
+      animal,
+      whale
+    ];
 
     Animal.remove({}, function(err){
         if (err) console.error(err);
-        elephant.save(function(err){
+        Animal.create(animalData, function(err, animals){
             if (err) console.error(err);
-            animal.save(function(err){
-                if (err) console.error(err);
-                whale.save(function(err){
-                    if (err) console.error(err);
-                    Animal.find({size: "big"}, function(err, animals) {
-                        animals.forEach(function(animal){
-                            console.log(animal.name + " the " + animal.color +
-                                " " + animal.type);
-                        });
-                        db.close(function(){
-                        console.log("db connection closed");
-                        });
-                    });
+            Animal.find({size: "big"}, function(err, animals) {
+                animals.forEach(function(animal){
+                    console.log(animal.name + " the " + animal.color +
+                        " " + animal.type);
+                });
+                db.close(function(){
+                    console.log("db connection closed");
                 });
             });
         });
